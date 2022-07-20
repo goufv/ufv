4e7966ff213171da31ece7c212e1648f37730c8c
SiteBuilderOne <anton@sitebuilderone.com>
2022-07-20 11:30:05 -0700

    t4 banner

--- html/cta-banner/marieme_files/ufv-ac.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("select.selectpicker").change(function(){
-	var selectedOption = $(".selectpicker option:selected").val();
-	//alert("You have selected: " + selectedOption);
-	window.location.replace("http://ufv.ca/");
-	window.location.href = selectedOption;
-	});
-});
